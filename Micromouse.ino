171bfed93adcce8fefd99b778d339353cb2754ab
Catherine <catherine.matta22399@gmail.com>
2019-11-19 16:54:06 -0500

    This is what we have so far, the second part is in progress

--- Micromouse.ino
+++ Micromouse.ino
@@ -11,18 +11,54 @@ motor2.setSpeed(120);
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
-  motor1.run(FORWARD);
- motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 1
+  //GO FORWARD
+    motor1.run(BACKWARD); // flip directions
+   motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 1
   delay(5000);
-
+//STOP the motor
   motor1.run(RELEASE);
-motor2.run(RELEASE);
-//delay(5000);
-
-  motor1.run(BACKWARD);//this makes it go backward
-  motor2.run(FORWARD);
+  motor2.run(RELEASE);
   delay(5000);
-
+//GO BACKWARD
+   motor1.run(FORWARD);//this makes it go in the same direction of the other one
+   motor2.run(FORWARD);
+  delay(5000);
+//GO FORWARD AGAIN
+   motor1.run(BACKWARD);//
+   motor2.run(FORWARD);
+   delay(1500);
+   motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  delay(1500);
+//GO RIGHT
+  motor1.setSpeed(-120);//opposite direction makes the motor turn?
+  motor2.setSpeed(120); 
+  motor1.run(BACKWARD);//
+  motor2.run(FORWARD);
+ delay(1500);
+// Go BACKWARD
+  motor1.setSpeed(120);
+  motor2.setSpeed(120);
+  motor1.run(BACKWARD); // flip directions
+  motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 
+ delay(5000);
+ //GO LEFT AND RIGHT -- IN PROGRESS
+   motor1.setSpeed(100);
+  motor2.setSpeed(100);
+   motor1.run(BACKWARD);//
+   motor2.run(BACKWARD);
+   delay(500);
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+   delay(500);
+  motor1.run(BACKWARD);//
+  motor2.run(FORWARD);
+  delay(500);
+  //Change of direction
+    motor1.run(FORWARD);//
+  motor2.run(BACKWARD);
+  delay(500);
  
 }
