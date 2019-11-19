15a76d7261b6258624911047c07c13bd89065266
Catherine <catherine.matta22399@gmail.com>
2019-11-19 12:46:24 -0500

    Made the motors run. added turning, and made it stop

--- Micromouse.ino
+++ Micromouse.ino
@@ -6,18 +6,19 @@ AF_DCMotor motor1(1,MOTOR12_64KHZ);
 AF_DCMotor motor2(2,MOTOR12_64KHZ);
 void setup() {
   // put your setup code here, to run once:
-motor1.setSpeed(100);
-motor2.setSpeed(100);
+motor1.setSpeed(120);
+motor2.setSpeed(120);
   
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  motor1.run(BACKWARD);
-  motor2.run(FORWARD);
-  delay(5000);
   motor1.run(FORWARD);
-  motor2.run(FORWARD);
-
+ motor2.run(BACKWARD);
+  delay(10000);
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+ motor1.run(RELEASE);// this stops the 1st motor
+ motor2.run(RELEASE); // this stops the 2nd motor
 }
