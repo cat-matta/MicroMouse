a8d7050dcfe4b483607797ffd84082f267a7ec3d
Catherine <catherine.matta22399@gmail.com>
2019-11-19 13:44:19 -0500

    Go forward and backward :D!

--- Micromouse.ino
+++ Micromouse.ino
@@ -7,18 +7,23 @@ AF_DCMotor motor2(2,MOTOR12_64KHZ);
 void setup() {
   // put your setup code here, to run once:
 motor1.setSpeed(120);
-motor2.setSpeed(120);
-  
+motor2.setSpeed(120);  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   motor1.run(FORWARD);
- motor2.run(BACKWARD);
-  delay(10000);
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
- motor1.run(RELEASE);// this stops the 1st motor
- motor2.run(RELEASE); // this stops the 2nd motor
+ motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 1
+  delay(5000);
+
+  motor1.run(RELEASE);
+motor2.run(RELEASE);
+//delay(5000);
+
+  motor1.run(BACKWARD);//this makes it go backward
+  motor2.run(FORWARD);
+  delay(5000);
+
+ 
 }
