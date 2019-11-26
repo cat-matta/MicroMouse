4e6e3418de0dc8d259dd8b67c4cd35a49bd00df2
Catherine <catherine.matta22399@gmail.com>
2019-11-26 14:51:48 -0500

    Added code for the sensor, the robot stops if the distance is less than 10cm

--- Micromouse.ino
+++ Micromouse.ino
@@ -1,30 +1,60 @@
 #include <AFMotor.h>
-
+#include <NewPing.h>
 //https://learn.adafruit.com/adafruit-motor-shield/using-dc-motors
 //https://learn.adafruit.com/adafruit-motor-shield/af-dcmotor-class
+//http://sribasu.com/programming-tutorials/creating-an-obstacle-avoiding-robot-with-arduino-adafruit-motor-shield-v1-and-ultra-sonic-sensor.html
+//https://www.tutorialspoint.com/arduino/arduino_ultrasonic_sensor.htm
+
 AF_DCMotor motor1(1,MOTOR12_64KHZ);
 AF_DCMotor motor2(2,MOTOR12_64KHZ);
+
+const int pingPin = A4; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = A5; // Echo Pin of Ultrasonic Sensor
+
 void setup() {
   // put your setup code here, to run once:
 motor1.setSpeed(120);
 motor2.setSpeed(120);  
+Serial.begin(9600); // Starting Serial Terminal
+
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
   //GO FORWARD
+  long duration, cm; // and inches ?
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);//THIS IS WHAT's Measuring our 
+//   inches = microsecondsToInches(duration);
+   cm = microsecondsToCentimeters(duration);
+   //Serial.print(inches);
+   //Serial.print("in, ");
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   //delay(1);
     motor1.run(BACKWARD); // flip directions
    motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 1
-  delay(5000);
+//  delay(5000);
 //STOP the motor
+  
+ // delay(5000);
+//GO BACKWARD
+if(cm<=10)
+{
   motor1.run(RELEASE);
   motor2.run(RELEASE);
-  delay(5000);
-//GO BACKWARD
-   motor1.run(FORWARD);//this makes it go in the same direction of the other one
-   motor2.run(FORWARD);
-  delay(5000);
+  /*motor1.run(FORWARD);//this makes it go in the same direction of the other one
+  motor2.run(FORWARD);*/
+}
+  /*delay(5000);
 //GO FORWARD AGAIN
    motor1.run(BACKWARD);//
    motor2.run(BACKWARD);
@@ -33,9 +63,9 @@ void loop() {
   motor2.run(RELEASE);
   delay(1500);
 //GO RIGHT I think?
-  motor1.setSpeed(-120);//opposite direction makes the motor turn?
+  motor1.setSpeed(120);//opposite direction makes the motor turn?
   motor2.setSpeed(120); 
-  motor1.run(BACKWARD);//
+  motor1.run(RELEASE);//
   motor2.run(BACKWARD);
  delay(1500);
 // Go BACKWARD
@@ -44,24 +74,14 @@ void loop() {
   motor1.run(BACKWARD); // flip directions
   motor2.run(BACKWARD);//MOTOR 2 runs opposite of motor 
  delay(5000);
+ */
 
-  /*
- //GO LEFT AND RIGHT -- IN PROGRESS
-   motor1.setSpeed(100);
-  motor2.setSpeed(100);
-   motor1.run(BACKWARD);//
-   motor2.run(BACKWARD);
-   delay(500);
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-   delay(500);
-  motor1.run(BACKWARD);//
-  motor2.run(FORWARD);
-  delay(500);
-  //Change of direction
-    motor1.run(FORWARD);//
-  motor2.run(BACKWARD);
-  delay(500);
-  */
- 
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
 }
